d46c4ef6398be0c82a5b4b95f9eae7367ee32d00
Nick Roessler <nicholas.e.roessler@gmail.com>
2025-07-15 20:57:23 -0400

    add support for privilege weight file; add example with weights

--- README.md
+++ README.md
@@ -37,8 +37,8 @@ graph LR
 
 %% SD1
 subgraph SD1[SD1]
-  F1[**F1**<br>Size: 20<br>Reads: 3<br >Writes: 5<br>Calls: 2<br >Returns: 2]
-  F2[**F2**<br>Size: 30<br>Reads: 4<br >Writes: 4<br>Calls: 0<br >Returns: 1]
+  F1[**F1**<br>Size: 20<br>Reads: 3<br >Writes: 0<br>Calls: 2<br >Returns: 2]
+  F2[**F2**<br>Size: 30<br>Reads: 4<br >Writes: 0<br>Calls: 0<br >Returns: 1]
 end
 style SD1 fill:transparent,stroke:black
 style F1 fill:#bbffbb,stroke:transparent
@@ -46,7 +46,7 @@ style F2 fill:#bbffbb,stroke:transparent
 
 %% SD2
 subgraph SD2[SD2]
-  F3[**F3**<br>Size: 16<br>Reads: 5<br >Writes: 1<br>Calls: 1<br >Returns: 1]
+  F3[**F3**<br>Size: 16<br>Reads: 5<br >Writes: 3<br>Calls: 1<br >Returns: 1]
 end
 style SD2 fill:transparent,stroke:black
 style F3 fill:#bbffbb,stroke:transparent
@@ -78,45 +78,92 @@ Here we show how to calculate the privilege of the compartmentalization.
 *Principal SD1*
 
 Read privilege: (3 + 4) x (10) = 70<br>
-Write privilege: (5 + 4) x (0) = 0<br>
+Write privilege: (0 + 0) x (0) = 0<br>
 Call privilege: (2 + 0) x (20 + 30 + 16) = 132<br>
 Return privilege: (2 + 1) x (20 + 30) = 150<br>
 
 *Principal SD2*
 
 Read privilege: (5) x (10) = 50<br>
-Write privilege: (1) x (20) = 20<br>
+Write privilege: (3) x (20) = 60<br>
 Call privilege: (1) x (16) = 16<br>
 Return privilege: (1) x (20 + 30 + 16) = 66<br>
 
 *Total Privilege of Compartmentalization (SD1 + SD2)*:
 
-Read: 120<br>
-Write: 20<br>
-Call: 148<br>
-Return: 216<br>
+Read: 70 + 50 = 120<br>
+Write: 0 + 60 = 60<br>
+Call: 132 + 16 = 148<br>
+Return: 150 + 66 = 216<br>
 
 ### Calculating PSR
-To calculate the PSR, we must first calculate the monolithic privilege. In the monolithic case, all instructions of each type can access all objects.
+To calculate the PSR, we must first calculate the monolithic privilege. In the monolithic case, all instructions of each type can access all objects. We assume no W^X memory permissions, meaning it is possible to read/write code or call into data. There is a flag in the privilege calculator for assuming W^X.
 
 *Monolithic Privilege*<br>
-Read: (3 + 4 + 5) x (10 + 20) = 360<br>
-Write: (5 + 4 + 1) x (10 + 20) = 300<br>
-Call: (2 + 0 + 1) x (20 + 30 + 16) = 198<br>
-Return: (2 + 1 + 1) x (20 + 30 + 16) = 264<br>
+Read: (3 + 4 + 5) x (10 + 20 + 16 + 20 + 30) = 1,152<br>
+Write: (0 + 0 + 3) x (10 + 20 + 16 + 20 + 30) = 288<br>
+Call: (2 + 0 + 1) x (10 + 20 + 16 + 20 + 30) = 288<br>
+Return: (1 + 2 + 1) x (10 + 20 + 16 + 20 + 30) = 384<br>
 
 The Privilege Set Ratio (PSR) is defined as the privilege permitted by a compartmentalization divided by the monolithic privilege.
 
 *PSR:*<br>
-Read PSR: 120 / 360 = **0.333**<br>
-Write PSR: 20 / 300 = **0.067**<br>
-Call PSR: 148 / 198 = **0.745**<br>
-Return PSR: 216 / 264 = **0.818**<br>
-Total PSR: 504 / 1,122 = **0.449**<br>
+Read PSR: 120 / 1,152 = **0.1042**<br>
+Write PSR: 60 / 288 = **0.2083**<br>
+Call PSR: 148 / 288 = **0.5138**<br>
+Return PSR: 216 / 384 = **0.5625**<br>
+Total PSR: 544 / 2,112 = **0.2576**<br>
+
+## Example with weight
+The privilege calculator can optionally be provided with a weight file which can be used to augment or overwrite privileges on the code and objects in the system. For example, weight can be used to mark an object as very important (or as not important).  See [CPM IF Specification Appendix B](https://github.com/SEVER-PROTECT/CPM-Interchange-Format) for the weight file syntax.
+
+To illustrate, consider applying a write weight of 10 to the object O2. Note that this is equivalent to settings its size to 200, but only for the write operation calculation.
+
+Calculating the privileges with this weight gives us the following:
+
+*Principal SD1 (same as before)*
+
+Read privilege: (3 + 4) x (10) = 70<br>
+Write privilege: (0 + 0) x (0) = 0<br>
+Call privilege: (2 + 0) x (20 + 30 + 16) = 132<br>
+Return privilege: (2 + 1) x (20 + 30) = 150<br>
+
+*Principal SD2 (write changed)*
+
+Read privilege: (5) x (10) = 50<br>
+Write privilege: (3) x (20x10) = 600<br>
+Call privilege: (1) x (16) = 16<br>
+Return privilege: (1) x (20 + 30 + 16) = 66<br>
+
+*Total Privilege of Compartmentalization (SD1 + SD2)*:
+
+Read: 70 + 50 = 120<br>
+Write: 0 + 600 = 600<br>
+Call: 132 + 16 = 148<br>
+Return: 150 + 66 = 216<br>
+
+*Monolithic Privilege*<br>
+Read: (3 + 4 + 5) x (10 + 20 + 16 + 20 + 30) = 1,152<br>
+Write: (0 + 0 + 3) x (10 + 20x10 + 16 + 20 + 30) = 828<br>
+Call: (2 + 0 + 1) x (10 + 20 + 16 + 20 + 30) = 288<br>
+Return: (1 + 2 + 1) x (10 + 20 + 16 + 20 + 30) = 384<br>
+
+*PSR:*<br>
+Read PSR: 120 / 1,152 = **0.1042**<br>
+Write PSR: 600 / 828 = **0.7246**<br>
+Call PSR: 148 / 288 = **0.5138**<br>
+Return PSR: 216 / 384 = **0.5625**<br>
+Total PSR: 1,084 / 2,652 = **0.4087**<br>
+
 
 ## Setup and Usage
 
-`calculate_privilege.py` requires the `pyyaml` package. Install it with `pip3 install -r requirements.txt`.
+`calculate_privilege.py` requires the `pyyaml` and `argparse` packages. Install them with `pip3 install -r requirements.txt`.
+
+Use with `python3 calculate_privilege.py <yaml> [--weight-file=file] [--wxorx | --no-wxorx]`
+
+An input yaml in the CPM IF specification is required. The `--weight-file` flag is optional and may be used to modify privileges of individual elements of the system on per-operation type granularity. The `--wxorx` and `--nowxorx` can be used to set whether Write-XOR-Execute (W^X) memory permissions are assumed in monolithic privilege calculations.
+
 
 When run on the included `example.yaml` file it produces the same results as our manual calculation:
 
@@ -128,4 +175,17 @@ WRITE   0.06666666666666667
 CALL    0.7474747474747475
 RETURN  0.8181818181818182
 TOTAL   0.44919786096256686
+```
+
+When run on the included `example.yaml` with setting the write weight of O2 to 10 using `weights.csv` it produces the same results as our manual calculation:
+
+```
+
+$ python3 calculate_privilege.py example.yaml --weight-file=weights.csv
+PSR: 
+READ	0.10416666666666667
+WRITE	0.7246376811594203
+CALL	0.5138888888888888
+RETURN	0.5625
+TOTAL	0.40874811463046756
 ```
--- calculate_privilege.py
+++ calculate_privilege.py
@@ -30,14 +30,22 @@ import argparse
 ops = ["READ", "WRITE", "CALL", "RETURN"]
 
 # Calculate the privilege of a given compartmentalization c and whether or not to assume W-XOR-X
-def calculate_privilege(c, wxorx):
+def calculate_privilege(c, weights, wxorx):
 
     print(f"Write-XOR-Execute: {wxorx}")
     subjs = []
     objs = []
-    subj_sizes = {}
-    obj_sizes = {}
     subj_op_counts = {}
+
+    obj_sizes = {}
+    subj_sizes = {}
+
+    # Weight by operation type
+    subj_weights = {}
+    obj_weights = {}
+    for op in ops:
+        subj_weights[op] = {}
+        obj_weights[op] = {}
 
     privilege = {}
     for op in ops:
@@ -49,6 +57,11 @@ def calculate_privilege(c, wxorx):
         subjects = subj_descriptor["subjects"]
         sizes = subj_descriptor.get("sizes", [1] * len(subjects))
         subj_sizes[subj] = sum(sizes)
+        for op in ops:
+            subj_weights[op][subj] = 0
+            for i in range(0, len(sizes)):
+                subj_weights[op][subj] += apply_weight(sizes[i], weights[op].get(subjects[i], "1"))
+
         num_funcs = len(subjects)
 
         subj_op_counts[subj] = {}
@@ -56,47 +69,52 @@ def calculate_privilege(c, wxorx):
         subj_op_counts[subj]["WRITE"] = subj_descriptor.get("write_instructions", num_funcs)
         subj_op_counts[subj]["CALL"] = subj_descriptor.get("call_instructions", num_funcs)
         subj_op_counts[subj]["RETURN"] = subj_descriptor.get("return_instructions", num_funcs)
-        
+
     for obj_descriptor in c['object_map']:
         obj = obj_descriptor['name']
         objs.append(obj)
         objects = obj_descriptor["objects"]
         sizes = obj_descriptor.get("sizes", [1] * len(subjects))
         obj_sizes[obj] = sum(sizes)
-        
+        for op in ops:
+            obj_weights[op][obj] = 0
+            for i in range(0, len(sizes)):
+                weight = apply_weight(sizes[i], weights[op].get(objects[i], "1"))
+                obj_weights[op][obj] += weight
+
     for principal in c['privileges']:
         subject = principal['principal']['subject']
 
-        privilege["CALL"] += subj_op_counts[subject]["CALL"] * subj_sizes[subject]
+        privilege["CALL"] += subj_op_counts[subject]["CALL"] * subj_weights["CALL"][subject]
         for call_domain in principal['can_call']:
             if call_domain == subject:
                 continue
-            if call_domain in subj_sizes:
-                privilege["CALL"] += subj_op_counts[subject]["CALL"] * subj_sizes[call_domain]
+            if call_domain in subj_op_counts:
+                privilege["CALL"] += subj_op_counts[subject]["CALL"] * subj_weights["CALL"][call_domain]
             else:
                 print(f"Subject {can_call} not found")
 
-        privilege["RETURN"] += subj_op_counts[subject]["RETURN"] * subj_sizes[subject]
+        privilege["RETURN"] += subj_op_counts[subject]["RETURN"] * subj_weights["RETURN"][subject]
         for return_domain in principal['can_return']:
             if return_domain == subject:
                 continue
-            if return_domain in subj_sizes:
-                privilege["RETURN"] += subj_op_counts[subject]["RETURN"] * subj_sizes[return_domain]
+            if return_domain in subj_op_counts:
+                privilege["RETURN"] += subj_op_counts[subject]["RETURN"] * subj_weights["RETURN"][return_domain]
                 
             else:
                 print(f"Subject {return_domain} not found")
 
         for read_descriptor in principal['can_read']:
             for obj_domain in read_descriptor['objects']:
-                if obj_domain in obj_sizes:
-                    privilege["READ"] += subj_op_counts[subject]["READ"] * obj_sizes[obj_domain]
+                if obj_domain in obj_weights["READ"]:
+                    privilege["READ"] += subj_op_counts[subject]["READ"] * obj_weights["READ"][obj_domain]
                 else:
                     print(f"Object {obj_domain} not found")
                     
         for write_descriptor in principal['can_write']:
             for obj_domain in write_descriptor['objects']:
-                if obj_domain in obj_sizes:
-                    privilege["WRITE"] += subj_op_counts[subject]["WRITE"] * obj_sizes[obj_domain]
+                if obj_domain in obj_weights["READ"]:
+                    privilege["WRITE"] += subj_op_counts[subject]["WRITE"] * obj_weights["WRITE"][obj_domain]
                 else:
                     print(f"Object {obj_domain} not found")
 
@@ -105,31 +123,83 @@ def calculate_privilege(c, wxorx):
     for op in ops:
         mono_priv[op] = 0
 
-    all_code_size = sum(subj_sizes.values())
-    all_data_size = sum(obj_sizes.values())
-    all_combined_size = all_code_size + all_data_size
-    
-    total_read_instrs = sum(s["READ"] for s in subj_op_counts.values())
-    total_write_instrs = sum(s["WRITE"] for s in subj_op_counts.values())
-    total_call_instrs = sum(s["CALL"] for s in subj_op_counts.values())
-    total_return_instrs = sum(s["RETURN"] for s in subj_op_counts.values())
+    all_data_size = {}
+    all_code_size = {}
 
-    if wxorx:
-        mono_priv["CALL"] = total_call_instrs * all_code_size
-        mono_priv["RETURN"] = total_return_instrs * all_code_size
-        mono_priv["READ"] = total_read_instrs * all_data_size
-        mono_priv["WRITE"] = total_write_instrs * all_data_size
-    else:
-        mono_priv["CALL"] = total_call_instrs * all_combined_size
-        mono_priv["RETURN"] = total_return_instrs * all_combined_size
-        mono_priv["READ"] = total_read_instrs * all_combined_size
-        mono_priv["WRITE"] = total_write_instrs * all_combined_size
+    for op in ops:
+        all_data_size[op] = 0
+        all_code_size[op] = 0
 
+        for s in subj_weights[op]:
+            all_code_size[op] += subj_weights[op][s]
+        for o in obj_weights[op]:
+            all_data_size[op] += obj_weights[op][o]
+
+    for principal in c['privileges']:
+        subject = principal['principal']['subject']
+        mono_priv["READ"] += subj_op_counts[subject]["READ"] * all_data_size["READ"]
+        mono_priv["WRITE"] += subj_op_counts[subject]["WRITE"] * all_data_size["WRITE"]
+        mono_priv["CALL"] += subj_op_counts[subject]["CALL"] * all_code_size["CALL"]
+        mono_priv["RETURN"] += subj_op_counts[subject]["RETURN"] * all_code_size["RETURN"]
+
+        # With no W^X, you can read/write code and call/return data
+        if not wxorx:
+            mono_priv["READ"] += subj_op_counts[subject]["READ"] * all_code_size["READ"]
+            mono_priv["WRITE"] += subj_op_counts[subject]["WRITE"] * all_code_size["WRITE"]
+            mono_priv["CALL"] += subj_op_counts[subject]["CALL"] * all_data_size["CALL"]
+            mono_priv["RETURN"] += subj_op_counts[subject]["RETURN"] * all_data_size["RETURN"]
+
+    #print(f"Priv: {privilege}")
+    #print(f"Mono Priv: {mono_priv}")
     PSR = calculate_PSR(privilege, mono_priv)
 
     print("PSR: ")
     for key in PSR:
         print(f"{key}\t{PSR[key]}")
+
+# Read weight file and return a dict indexed by subj/obj, and op, and then holds a weight
+def parse_weight_file(filename):
+
+    try:
+        f = open(filename, "r")
+    except Exception as e:
+        print("Unable to open file " + filename)
+        return {}
+
+    weights = {}
+    for op in ops:
+        weights[op] = {}
+
+    lines = f.readlines()
+    weights_found = 0
+    for l in lines:
+        l = l.strip()
+        if len(l) == 0:
+            continue
+        if l[0] == "#":
+            continue
+        fields = l.split(",")
+        if len(fields) != 5:
+            raise Exception(f"Invalid weight file, did not have 5 fields on line '{l}'")
+        entity_id = fields[0].strip()
+        weights["READ"][entity_id] = fields[1].strip()
+        weights["WRITE"][entity_id] = fields[2].strip()
+        weights["CALL"][entity_id] = fields[3].strip()
+        weights["RETURN"][entity_id] = fields[4].strip()
+        weights_found += 1
+
+    print(f"Read {weights_found} privilege weights from {filename}")
+    return weights
+
+def apply_weight(value, weight_string):
+
+    if weight_string == "":
+        return value
+
+    if weight_string[0] == "=":
+        return value * int(weight_string[1:])
+    else:
+        return value * int(weight_string)
 
 def calculate_PSR(comp_priv, mono_priv):
 
@@ -147,7 +217,6 @@ def calculate_PSR(comp_priv, mono_priv):
     result["TOTAL"] = total_comp_priv / total_mono_priv
 
     return result
-    
 
 if __name__ == '__main__':
 
@@ -155,22 +224,33 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument('compfile', help='Compartmentalization YAML file')
-    parser.add_argument('--no-wxorx', action=argparse.BooleanOptionalAction, help="Removes the assumption of write-xor-execute memory permissions")
+    parser.add_argument('--weight-file', help="Additional weight csv file")
+    parser.add_argument('--wxorx', action=argparse.BooleanOptionalAction, help="Adds the assumption of write-xor-execute memory permissions", default=False)
 
     args = parser.parse_args()
-    wxorx = args.no_wxorx != False
+    wxorx = args.wxorx != False
+
+    # See if we have a weight file and parse it
+    if args.weight_file != None:
+        weights = parse_weight_file(args.weight_file)
+    else:
+        weights = {}
+        for op in ops:
+            weights[op] = {}
 
     # Open the file
     try:
         f = open(args.compfile, 'r')
     except Exception as e:
-        print("Unable to open file " + args.compfile)
+        print("Error: Unable to open file " + args.compfile)
+        exit()
 
     # Parse as yaml
     try:
         c = yaml.safe_load(f)
     except yaml.YAMLError as e:
         print("Error parsing yaml file: " + args.compfile)
-    
+        exit()
+
     # Calculate privileges on it
-    calculate_privilege(c, wxorx)
+    calculate_privilege(c, weights, wxorx)
